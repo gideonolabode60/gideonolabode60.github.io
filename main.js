39a6cef67ec17c92802e132822c836891c639b8c
gideonolabode60 <gideonolabode60@gmail.com>
2026-07-19 23:22:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -30,3 +30,11 @@ function changeText() {
 }
 
 setInterval(changeText, 3000);
+// Mobile Menu
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
